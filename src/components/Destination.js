7e79b4f16aaca4e2fca1c2c8d194cf19f5fcca1b
barinder <barindersingh1997@gmail.com>
2023-05-28 14:36:06 +0530

    commit

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -6,7 +6,7 @@ const Destination=()=>{
             <h1>Popular Destinations</h1>
             <p>Tours Give You The Opportunity To See A Lot,Witnin A Time Frame.</p>
            
-            <DestinationData classname="first-des" heading="Destination name here" text="rtr is a ncdjncj.knmdijfedef fefef,htyth,hterthg tgte eghtgh etgetgtte gtetgtegrt rteggert,
+            <DestinationData classname="first-des" heading="Destination name here" text="rtr is a ncdjncj.knmdijffedef fefef,htyth,hterthg tgte eghtgh etgetgtte gtetgtegrt rteggert,
             so thstds why its a best place to visit either in summers ie , winters a well,as oyu akk nirfu aeburg ainfgreu iefjgi ihu ueheuh efnj ekffej fejnefuhue , lokfeni wubfuy fuirhur frijf rfnjurh wfnurhfu jfhu frijhr ihfrnfriu uh" img1="https://i.pinimg.com/originals/28/12/13/281213cfcc0bf4b51c0bb384454fa330.jpg" img2="https://wallpapers.net/supernatural-peace-hd-wallpaper/download/1536x2048.jpg"/>    
             
             <DestinationData classname="first-des-reverse" heading="Destination second here" text="rtr is a ncdjncj.knmdijfedef fefef,htyth,hterthg tgte eghtgh etgetgtte gtetgtegrt rteggert,
