cf710b638da7f4fafb5f35028bd0ade8ebea7452
barinder <barindersingh1997@gmail.com>
2023-05-28 15:21:31 +0530

    rotes fixed

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
           <Routes>
              <Route path="/" element={<Home/>} ></Route>
              <Route path="/about" element={<About/>} ></Route>
-             <Route path="/contact" element={<Contact/>} />
-             <Route path="/service" element={<Service/>} />
+             <Route path="/contact" element={<Contact/>} ></Route>
+             <Route path="/service" element={<Service/>}></Route>
           </Routes>
       </BrowserRouter>
     </div>
