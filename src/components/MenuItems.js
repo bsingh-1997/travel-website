e8c79ac68b2c982db0bf496f1ee6030214d25895
barinder <barindersingh1997@gmail.com>
2023-06-01 05:51:07 +0530

    url changes

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,28 +1,28 @@
 export const MenuItems = [
     {
         title:'Home',
-        url :"/",
+        url :"/travel-website",
         cName:"nav-s",
         icon:"fa-solid fa-house"
     },
     
     {
         title:'About',
-        url :"/#/about",
+        url :"/about",
         cName:"nav-s",
         icon:"fa-solid fa-circle-info"
     },
     
     {
         title:'Service',
-        url :"/#/service",
+        url :"/service",
         cName:"nav-s",
         icon:"fa-solid fa-briefcase"
     },
     
     {
         title:'Contact',
-        url :"/#/contact",
+        url :"/contact",
         cName:"nav-s",
         icon:"fa-solid fa-address-book"
     },
